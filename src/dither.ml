a8032669938f176dbc8354dc1bc68fcc39a55ff5
Anoushka <anoushka.m.arora@gmail.com>
2023-07-05 20:59:57 +0000

    dither so far

--- src/dither.ml
+++ src/dither.ml
@@ -1,7 +1,29 @@
 open Core
 
+let valid_coord x y image = 
+  let height = Image.height image in
+  let width = Image.width image in
+  if(x < width && y < height) then true else false
+;; 
+
 (* This should look familiar by now! *)
-let transform image = image
+let transform image = 
+  let gray_img = Grayscale.transform image in 
+  let transform_pix ~x ~y pixel = 
+    let value = Pixel.red pixel in
+    let error = (match (value > 128) with
+    | true -> 255 - value
+    | false -> 0 - value) in 
+    let error = Float.of_int error in 
+    let right_weight = Float.( * ) (Float.(/) 7.0 16.0) error in 
+    let sw_weight = Float.( * ) (Float.(/) 3.0 16.0) error in 
+    let down_weight = Float.( * ) (Float.(/) 5.0 16.0) error in
+    let se_weight = Float.( * ) (Float.(/) 1.0 16.0) error in 
+    if(valid_coord (x+1) y image) then (Image.set ~x: (x + 1) ~y (Image))
+  in 
+
+
+
 
 let command =
   Command.basic
