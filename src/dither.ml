6581c3cb51b39560bf444885755ac077779ce115
Anoushka <anoushka.m.arora@gmail.com>
2023-07-07 18:09:47 +0000

    dither edits

--- src/dither.ml
+++ src/dither.ml
@@ -6,27 +6,39 @@ let valid_coord x y image =
   if x < width && y < height && y >= 0 && x >= 0 then true else false
 ;;
 
-let get_board_positions image =
-  let height = Image.height image in
-  let width = Image.width image in
-  let positions =
-    List.init width ~f:(fun x -> List.init height ~f:(fun y -> x, y))
+let get_contrasted_image image =
+  let final_transform ~x ~y image pixel : Image.t =
+    let value = Pixel.red pixel in
+    let _b =
+      match
+        Float.( < )
+          0.5
+          (Float.( / )
+             (Float.of_int value)
+             (Float.of_int (Image.max_val image)))
+      with
+      | true -> Image.set image ~x ~y (Pixel.of_int (Image.max_val image))
+      | false -> Image.set image ~x ~y (Pixel.of_int 0)
+    in
+    image
   in
-  List.concat positions
+  Image.foldi image ~init:image ~f:final_transform
 ;;
 
 (* This should look familiar by now! *)
 let transform image =
+  (*let transformed_image = get_contrasted_image image in *)
   let image = Grayscale.transform image in
-  let transform_pix ~x ~y pixel =
+  let transformed_img = get_contrasted_image image in
+  let transform_pix ~x ~y img pixel =
     let value = Pixel.red pixel in
     let error =
-      match value > 128 with
+      match value > Image.max_val img / 2 with
       | true ->
-        Image.set image ~x ~y (255, 255, 255);
-        255 - value
+        Image.set img ~x ~y (Pixel.of_int (Image.max_val img));
+        Image.max_val img - value
       | false ->
-        Image.set image ~x ~y (0, 0, 0);
+        Image.set img ~x ~y (Pixel.of_int 0);
         0 - value
     in
     let error = Float.of_int error in
@@ -34,64 +46,48 @@ let transform image =
     let sw_weight = Float.( * ) (Float.( / ) 3.0 16.0) error in
     let down_weight = Float.( * ) (Float.( / ) 5.0 16.0) error in
     let se_weight = Float.( * ) (Float.( / ) 1.0 16.0) error in
-    if valid_coord (x + 1) y image
+    if valid_coord (x + 1) y img
     then
       Image.set
-        image
+        img
         ~x:(x + 1)
         ~y
         (Pixel.( + )
-           (Image.get image ~x:(x + 1) ~y)
-           ( Int.of_float right_weight
-           , Int.of_float right_weight
-           , Int.of_float right_weight ));
-    if valid_coord x (y - 1) image
+           (Image.get img ~x:(x + 1) ~y)
+           (Pixel.of_int (Int.of_float right_weight)));
+    if valid_coord x (y - 1) img
     then
       Image.set
-        image
+        img
         ~x
         ~y:(y - 1)
         (Pixel.( + )
-           (Image.get image ~x ~y:(y - 1))
-           ( Int.of_float down_weight
-           , Int.of_float down_weight
-           , Int.of_float down_weight ));
-    if valid_coord (x - 1) (y - 1) image
+           (Image.get img ~x ~y:(y - 1))
+           (Pixel.of_int (Int.of_float down_weight)));
+    if valid_coord (x - 1) (y - 1) img
     then
       Image.set
-        image
+        img
         ~x:(x - 1)
         ~y:(y - 1)
         (Pixel.( + )
-           (Image.get image ~x:(x - 1) ~y:(y - 1))
-           ( Int.of_float sw_weight
-           , Int.of_float sw_weight
-           , Int.of_float sw_weight ));
-    if valid_coord (x + 1) (y - 1) image
+           (Image.get img ~x:(x - 1) ~y:(y - 1))
+           (Pixel.of_int (Int.of_float sw_weight)));
+    if valid_coord (x + 1) (y - 1) img
     then
       Image.set
-        image
+        img
         ~x:(x + 1)
         ~y:(y - 1)
         (Pixel.( + )
-           (Image.get image ~x:(x + 1) ~y:(y - 1))
-           ( Int.of_float se_weight
-           , Int.of_float se_weight
-           , Int.of_float se_weight ));
-    let pix_val = Image.get image ~x ~y in
-    print_s [%message "Pix value: " (pix_val : Pixel.t)]
+           (Image.get img ~x:(x + 1) ~y:(y - 1))
+           (Pixel.of_int (Int.of_float se_weight)));
+    img
   in
-  let img_positions = get_board_positions image in
-  let rec dither_img ~(img_positions : (int * int) list) =
-    match img_positions with
-    | [] -> ()
-    | (x, y) :: tail ->
-      transform_pix ~x ~y (Image.get image ~x ~y);
-      dither_img ~img_positions:tail
-  in
-  dither_img ~img_positions;
-  image
+  Image.foldi transformed_img ~init:transformed_img ~f:transform_pix
 ;;
+
+(*Image.foldi image ~init:image ~f:transform_pix*)
 
 let command =
   Command.basic
